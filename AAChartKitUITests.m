6e329cb097f08eec9c589b23076a4c7e67368dc0
爱你不变 <1373513133@qq.com>
2018-06-25 12:50:17 +0800

    444
    444

--- AAChartKitUITests.m
+++ AAChartKitUITests.m
@@ -42,7 +42,7 @@
     [super setUp];
     
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    
+    sdaasdasdasdasdsadsadsadasdasd
     // In UI tests it is usually best to stop immediately when a failure occurs.
     self.continueAfterFailure = NO;
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
